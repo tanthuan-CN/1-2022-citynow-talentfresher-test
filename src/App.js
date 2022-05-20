aca31f1884de4157b426926aad0dab979cc5e1c3
huutricn <huutri.nguyen@m.citynow.vn>
2022-05-20 18:21:52 +0700

    Context For App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 // libs
 import React from "react";
+// contexts
+import { ProvideLocale } from "./contexts/LocalContext";
+import { ProvideControl } from "./contexts/ControlContext";
 // components
 import Home from "./pages/Home";
 // others
 import "./App.scss";
 
-const App = () => <Home />;
+const App = () => (
+  <ProvideControl>
+    <ProvideLocale>
+      <Home />
+    </ProvideLocale>
+  </ProvideControl>
+);
 
 export default App;
