23a1fb24c3b97fa1e5f4d64dec6eb400f7d1a0dd
huutricn <huutri.nguyen@m.citynow.vn>
2022-05-20 18:19:10 +0700

    Hide Checkout Room

--- src/pages/Home/components/RoomComponent/index.jsx
+++ src/pages/Home/components/RoomComponent/index.jsx
@@ -1,18 +1,30 @@
 // libs
 import React from "react";
+import classNames from "classnames";
+// contexts
+import { useControl } from "@/contexts/ControlContext";
 // others
 import "./styles.scss";
 import { roomStatus } from "@/constants";
 
-const RoomComponent = ({ personName, roomNo, roomType, night, status }) => (
-  <div className="room-wrapper" style={{ backgroundColor: roomStatus[status] }}>
-    <div className="room-data">
-      <h3 className="room-number">{roomNo}</h3>
-      <p className="room-type">{roomType}</p>
+const RoomComponent = ({ personName, roomNo, roomType, night, status }) => {
+  const { isHide } = useControl();
+
+  return (
+    <div
+      className={classNames("room-wrapper", {
+        hide: isHide && status === "ALREADY_OUT"
+      })}
+      style={{ backgroundColor: roomStatus[status] }}
+    >
+      <div className="room-data">
+        <h3 className="room-number">{roomNo}</h3>
+        <p className="room-type">{roomType}</p>
+      </div>
+      <div className="room-night">{night}</div>
+      <div className="room-guest">{personName}</div>
     </div>
-    <div className="room-night">{night}</div>
-    <div className="room-guest">{personName}</div>
-  </div>
-);
+  );
+};
 
 export default RoomComponent;
