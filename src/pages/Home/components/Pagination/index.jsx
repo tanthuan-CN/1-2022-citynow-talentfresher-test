5212195adac0177232d3e82451e2fc74610673e0
Binh Nguyen <ngvanbinh2001@gmail.com>
2022-05-20 17:02:40 +0700

    Create Pagination component

--- src/pages/Home/components/Pagination/index.jsx
+++ src/pages/Home/components/Pagination/index.jsx
@@ -2,11 +2,20 @@
 import React from "react";
 // styles
 import "./styles.scss";
+// others
+import { useLocale } from "@/contexts/LocaleContext";
 
-const Pagination = () => (
-  <div className="pagination-wrapper">
-    <div className="pagination-inner">PAGE: 3/3</div>
-  </div>
-);
+const Pagination = ({ pagination }) => {
+  const { localeDataSource } = useLocale();
+  const totalPage = Math.ceil(pagination.total / pagination.limit);
+
+  return (
+    <div className="pagination-wrapper">
+      <div className="pagination-inner">
+        {localeDataSource.page}: {pagination.page} / {totalPage}
+      </div>
+    </div>
+  );
+};
 
 export default Pagination;
