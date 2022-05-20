00851f1db6573097943b02a436493041dac86652
huutricn <huutri.nguyen@m.citynow.vn>
2022-05-20 18:02:49 +0700

    Add Locale

---
+++ src/locales/en.js
@@ -0,0 +1,11 @@
+const en = {
+  "Controls.PageDown": "F1",
+  "Controls.PageUp": "F3",
+  "Controls.ShowCheckout": "Show Checkout",
+  "Controls.HideCheckout": "Hide Checkout",
+  "Controls.Refresh": "Refresh",
+  "Controls.Vietnamese": "VI",
+  "Controls.English": "EN"
+};
+
+export default en;
---
+++ src/locales/vi.js
@@ -0,0 +1,9 @@
+const vi = {
+  "Controls.ShowCheckout": "Hiện Checkout",
+  "Controls.HideCheckout": "Ẩn Checkout",
+  "Controls.Refresh": "Làm mới",
+  "Controls.Vietnamese": "VI",
+  "Controls.English": "EN"
+};
+
+export default vi;
