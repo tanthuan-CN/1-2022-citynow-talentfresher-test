59fabdf71da86f7667bb164c1792392c9d1cd17f
huutricn <huutri.nguyen@m.citynow.vn>
2022-05-20 18:20:30 +0700

    Feature Pagination for home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,16 +3,28 @@ import React from "react";
 // components
 import Header from "./mains/Header";
 import RoomList from "./mains/RoomList";
+// hocks
+import usePagination from "@/hooks/usePagination";
+// mocks
+import { roomData } from "@/mocks";
 // others
 import "./style.scss";
+import { RoomList as ROOM_CONST } from "@/constants";
 
-const Home = () => (
-  <div className="home-wrapper">
-    <div className="home-wrapper-inner">
-      <Header />
-      <RoomList />
+const Home = () => {
+  const { start, end, totalPage, currentPage } = usePagination({
+    total: roomData.length,
+    itemPerPage: ROOM_CONST.ROOM_PER_PAGE
+  });
+
+  return (
+    <div className="home-wrapper">
+      <div className="home-wrapper-inner">
+        <Header totalPage={totalPage} currentPage={currentPage} />
+        <RoomList roomData={roomData.slice(start, end)} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
