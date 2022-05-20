897d29b1ee2b91c28ee1b05481a02d56335cf35b
huutriCitynow <huutri.nguyen@m.citynow.vn>
2022-05-20 11:35:22 +0700

    DataSource For Status and Buttons

---
+++ src/dataSources/ButtonList/index.js
@@ -0,0 +1,24 @@
+const buttonList = [
+  {
+    buttonName: "F1",
+    buttonState: "Down"
+  },
+  {
+    buttonName: "F3",
+    buttonState: "Up"
+  },
+  {
+    buttonName: "F4",
+    buttonState: "Show Checkout"
+  },
+  {
+    buttonName: "F5",
+    buttonState: "Refresh"
+  },
+  {
+    buttonName: "F12",
+    buttonState: "VI"
+  }
+];
+
+export default buttonList;
