7c3b0283c8ee6133f8d8a290c3c208ce305c570c
Binh Nguyen <ngvanbinh2001@gmail.com>
2022-05-20 17:02:00 +0700

    Apply Prop Drilling for pagination and show checkout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,32 @@
 // libs
-import React from "react";
+import usePagination from "@/hooks/usePagination";
+import React, { useState } from "react";
 // styles
 import "./index.scss";
 // layouts
 import RoomControl from "./mains/RoomControl";
 import RoomView from "./mains/RoomView";
+// others
+import { roomData } from "@/mocks/index";
 
-const HomePage = () => (
-  <div className="container">
-    <RoomControl />
-    <RoomView />
-  </div>
-);
+const HomePage = () => {
+  const { pagination, handleChangePage: setPagination } = usePagination(
+    1,
+    140,
+    roomData.length
+  );
+  const [isShowCheckout, setIsShowCheckout] = useState(true);
+
+  return (
+    <div className="container">
+      <RoomControl
+        pagination={pagination}
+        showCheckout={isShowCheckout}
+        onChangePagination={setPagination}
+        onChangeShowCheckout={setIsShowCheckout}
+      />
+      <RoomView pagination={pagination} showCheckout={isShowCheckout} />
+    </div>
+  );
+};
 export default HomePage;
