65bd69c24642aa169b0b27352afc7a172ab691ca
huutricn <huutri.nguyen@m.citynow.vn>
2022-05-20 18:08:17 +0700

    DataSource for ButtonList

--- src/dataSources/ButtonList/index.js
+++ src/dataSources/ButtonList/index.js
@@ -1,23 +1,38 @@
+// libs
+import React from "react";
+import { CaretDownOutlined, CaretUpOutlined } from "@ant-design/icons";
+// component
+import PageUpButton from "@/pages/Home/components/PageUpButton";
+import PageDownButton from "@/pages/Home/components/PageDownButton";
+import { HideCheckoutButton } from "@/pages/Home/components/HideCheckoutButton";
+import RefreshButton from "@/pages/Home/components/RefreshButton";
+import LocaleButton from "@/pages/Home/components/LocaleButton";
+
 const buttonList = [
   {
     buttonName: "F1",
-    buttonState: "Down"
+    buttonState: <CaretDownOutlined />,
+    buttonType: <PageUpButton buttonName="F1" buttonState="Hello" />
   },
   {
     buttonName: "F3",
-    buttonState: "Up"
+    buttonType: <PageDownButton buttonName="F1" buttonState="Hello" />,
+    buttonState: <CaretUpOutlined />
   },
   {
     buttonName: "F4",
-    buttonState: "Show Checkout"
+    buttonType: <HideCheckoutButton buttonName="F1" buttonState="Hello" />,
+    buttonState: "Controls.HideCheckout"
   },
   {
     buttonName: "F5",
-    buttonState: "Refresh"
+    buttonType: <RefreshButton buttonName="F1" buttonState="Hello" />,
+    buttonState: "Controls.Refresh"
   },
   {
     buttonName: "F12",
-    buttonState: "VI"
+    buttonType: <LocaleButton buttonName="F1" buttonState="Hello" />,
+    buttonState: "Controls.Vietnamese"
   }
 ];
 
