2d1aa4793b9858ab62b153afa8f015549189225a
Binh Nguyen <ngvanbinh2001@gmail.com>
2022-05-20 11:38:02 +0700

    Create Pagination component

---
+++ src/pages/Home/components/Pagination/index.jsx
@@ -0,0 +1,12 @@
+// libs
+import React from "react";
+// styles
+import "./styles.scss";
+
+const Pagination = () => (
+  <div className="pagination-wrapper">
+    <div className="pagination-inner">PAGE: 3/3</div>
+  </div>
+);
+
+export default Pagination;
