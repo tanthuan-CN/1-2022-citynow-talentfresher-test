0b816d50ac6cd4b7edd9aaf9d8b0fa9022463922
Binh Nguyen <ngvanbinh2001@gmail.com>
2022-05-20 17:04:28 +0700

    Apply multi-language to StatusRoom component

--- src/pages/Home/components/StatusRoom/index.jsx
+++ src/pages/Home/components/StatusRoom/index.jsx
@@ -3,24 +3,27 @@ import React from "react";
 // styles
 import "./styles.scss";
 // others
-import { roomStatusList } from "@/dataSources/index";
+import { useLocale } from "@/contexts/LocaleContext";
 
-const StatusRoom = () => (
-  <div className="status-room-wrapper">
-    <div className="status-room-inner">
-      <ul className="status-room-list">
-        {roomStatusList.map((item) => (
-          <li className="status-room-item" key={item.status}>
-            <div
-              className="status-room-color"
-              style={{ background: item.color }}
-            ></div>
-            <div className="status-room-title">{item.status}</div>
-          </li>
-        ))}
-      </ul>
+const StatusRoom = () => {
+  const { localeDataSource } = useLocale();
+  return (
+    <div className="status-room-wrapper">
+      <div className="status-room-inner">
+        <ul className="status-room-list">
+          {localeDataSource.roomStatusList.map((item) => (
+            <li className="status-room-item" key={item.status}>
+              <div
+                className="status-room-color"
+                style={{ background: item.color }}
+              ></div>
+              <div className="status-room-title">{item.status}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default StatusRoom;
