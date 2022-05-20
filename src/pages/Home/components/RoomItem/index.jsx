8ff094830aba99081733b2ef1b991d2e6da9c285
Binh Nguyen <ngvanbinh2001@gmail.com>
2022-05-20 17:05:19 +0700

    Update Room View (List, Item) component

--- src/pages/Home/components/RoomItem/index.jsx
+++ src/pages/Home/components/RoomItem/index.jsx
@@ -5,17 +5,18 @@ import "./styles.scss";
 // others
 import { roomStatusList } from "@/mocks/index";
 
-const RoomItem = ({ status, no, personName, type, night }) => {
+const RoomItem = ({ status, no, personName, type, night, showCheckout }) => {
   const backgroundColor = roomStatusList.findIndex(
     (room) => room.status === status
   );
+  const roomItemStyles = {
+    visibility:
+      status === "ALREADY_OUT" && !showCheckout ? "hidden" : "visible",
+    background: roomStatusList[backgroundColor].color
+  };
 
   return (
-    <li
-      className="room-item"
-      style={{ background: roomStatusList[backgroundColor].color }}
-      title={personName}
-    >
+    <li className="room-item" style={roomItemStyles} title={personName}>
       <div className="room-info">
         <div className="room-id">{no}</div>
         <div className="room-desc">
--- src/pages/Home/components/RoomList/index.jsx
+++ src/pages/Home/components/RoomList/index.jsx
@@ -5,21 +5,29 @@ import "./styles.scss";
 // layouts
 import RoomItem from "../RoomItem";
 // others
-import { roomData } from "@/mocks/index";
+// import { roomData } from "@/mocks/index";
+import { useRoom } from "@/contexts/RoomContext";
 
-const RoomList = () => (
-  <ul className="room-list">
-    {roomData.slice(0, 140).map((room) => (
-      <RoomItem
-        key={room.id}
-        status={room.status}
-        no={room.roomNo}
-        personName={room.personName}
-        type={room.roomType}
-        night={room.night}
-      />
-    ))}
-  </ul>
-);
+const RoomList = ({ pagination, showCheckout }) => {
+  const startIndex = pagination.page * pagination.limit - pagination.limit;
+  const finalIndex = pagination.page * pagination.limit;
+  const { roomList: roomData } = useRoom();
+
+  return (
+    <ul className="room-list">
+      {roomData.slice(startIndex, finalIndex).map((room) => (
+        <RoomItem
+          key={room.id}
+          status={room.status}
+          no={room.roomNo}
+          personName={room.personName}
+          type={room.roomType}
+          night={room.night}
+          showCheckout={showCheckout}
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default RoomList;
