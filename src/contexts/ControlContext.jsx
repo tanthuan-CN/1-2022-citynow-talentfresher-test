50c2dec7c5e89e69f096dfa9d0f0d656deaa1b46
huutricn <huutri.nguyen@m.citynow.vn>
2022-05-23 11:30:49 +0700

    HandleKey for ControlContext

--- src/contexts/ControlContext.jsx
+++ src/contexts/ControlContext.jsx
@@ -8,6 +8,9 @@ const ControlContext = createContext();
 export const ProvideControl = ({ children }) => {
   const [isHide, setHide] = useState(false);
   const [isHideActive, setHideActive] = useState(false);
+  const [isRefreshActive, setRefreshActive] = useState(false);
+  const [isPageUp, setIsPageUp] = useState(false);
+  const [isPageDown, setIsPageDown] = useState(false);
 
   useEffect(() => {
     const handleKey = (e) => {
@@ -27,10 +30,76 @@ export const ProvideControl = ({ children }) => {
     return () => {
       window.removeEventListener("keydown", handleKey);
     };
-  }, []);
+  }, [isHide, isHideActive]);
+
+  useEffect(() => {
+    const handleKey = (e) => {
+      if (e.keyCode === keyCode.REFRESH) {
+        e.preventDefault();
+
+        setRefreshActive(true);
+
+        setTimeout(() => {
+          setRefreshActive(false);
+        }, Time.TIME_SHOW_HIDE);
+      }
+    };
+
+    window.addEventListener("keydown", handleKey);
+
+    return () => {
+      window.removeEventListener("keydown", handleKey);
+    };
+  }, [isRefreshActive]);
+
+  useEffect(() => {
+    const handleKey = (e) => {
+      if (e.keyCode === keyCode.PAGE_UP) {
+        e.preventDefault();
+
+        setIsPageUp(true);
+        setTimeout(() => {
+          setIsPageUp(false);
+        }, Time.TIME_SHOW_HIDE);
+      }
+    };
+
+    window.addEventListener("keydown", handleKey);
+
+    return () => {
+      window.removeEventListener("keydown", handleKey);
+    };
+  }, [isPageUp]);
+
+  useEffect(() => {
+    const handleKey = (e) => {
+      if (e.keyCode === keyCode.PAGE_DOWN) {
+        e.preventDefault();
+
+        setIsPageDown(true);
+        setTimeout(() => {
+          setIsPageDown(false);
+        }, Time.TIME_SHOW_HIDE);
+      }
+    };
+
+    window.addEventListener("keydown", handleKey);
+
+    return () => {
+      window.removeEventListener("keydown", handleKey);
+    };
+  }, [isPageDown]);
 
   return (
-    <ControlContext.Provider value={{ isHide, isHideActive, setHide }}>
+    <ControlContext.Provider
+      value={{
+        isHide,
+        isHideActive,
+        isRefreshActive,
+        isPageUp,
+        isPageDown
+      }}
+    >
       {children}
     </ControlContext.Provider>
   );
