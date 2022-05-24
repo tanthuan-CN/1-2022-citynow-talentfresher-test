50a1abb4bdd213b7f01213ea67e6c8d461cdefc0
huutricn <huutri.nguyen@m.citynow.vn>
2022-05-24 16:36:27 +0700

    Drop default Locale

--- src/pages/Home/mains/Header/index.jsx
+++ src/pages/Home/mains/Header/index.jsx
@@ -1,32 +1,22 @@
 // libs
-import React, { useEffect } from "react";
+import React from "react";
 // components
 import Pagination from "../../components/Pagination";
 import RoomStateList from "../../components/RoomStateList";
 import Controls from "../../components/Controls";
-// contexts
-import { useLocale } from "@/contexts/LocalContext";
 // others
 import "./styles.scss";
 
-const Header = ({ currentPage, totalPage }) => {
-  const { setLocale } = useLocale();
-
-  useEffect(() => {
-    setLocale("vi");
-  }, []);
-
-  return (
-    <div className="header-wrapper">
-      <div className="header-wrapper-inner">
-        <RoomStateList />
-        <Pagination currentPage={currentPage} totalPage={totalPage} />
-        <div className="header-controls">
-          <Controls />
-        </div>
+const Header = ({ currentPage, totalPage }) => (
+  <div className="header-wrapper">
+    <div className="header-wrapper-inner">
+      <RoomStateList />
+      <Pagination currentPage={currentPage} totalPage={totalPage} />
+      <div className="header-controls">
+        <Controls />
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Header;
